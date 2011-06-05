60462beec31320d46654d36c8c068d8ca51ae7b7
Jeff <jeff.vera@gmail.com>
2011-06-05 18:12:29 -0400

    - Renamed Random to RandomProcessor to avoid naming conflicts.

---
+++ TTT/AI/RandomProcessor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace TTT.AI
+{
+	public class RandomProcessor : IProcessor
+	{
+		public void MakeMove(Board b)
+		{
+			IList<Tuple<int, int>> moves = b.GetAvailableMoves();
+			if (moves.Count == 0) return;
+
+			RandomProcessor r = new RandomProcessor();
+			
+		}
+	}
+}
