5f90aa5a69c028f2161c06fcbcb644c29b6416cf
Jeff <jeff.vera@gmail.com>
2011-06-05 22:11:07 -0400

    - Beginnings of rudimentary minimax algorithm.

---
+++ TTT/AI/Minimax.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using TTT;
+
+namespace TTT.AI
+{
+	public class Minimax
+	{
+		public static int minimax(Board b, Piece source)
+		{
+			IList<Tuple<int, int>> moves = b.GetAvailableMoves();
+			if (moves.Count == 0)
+			{
+				Piece winner = b.GetWinner();
+				if (winner == null)
+				{
+					return 0;
+				}
+				else if (winner.Symbol == source.Symbol)
+				{
+					return 100;
+				}
+				else
+				{
+					return -100;
+				}
+			}
+			else
+			{
+				throw new NotImplementedException();
+			}
+		}
+	}
+}
---
+++ TTTTests/AI/MinimaxTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Moq;
+using TTT;
+using TTT.AI;
+
+namespace TTTTests.AI
+{
+	[TestFixture]
+	public class MinimaxTests
+	{
+		[Test]
+		public void DrawReturnsZeroTest()
+		{
+			Mock<Board> mockBoard = new Mock<Board> { CallBase = true };
+			mockBoard.Setup(x => x.GetAvailableMoves())
+				.Returns(new List<Tuple<int, int>>());
+
+			int returnValue = Minimax.minimax(mockBoard.Object, null);
+
+			Assert.That(returnValue, Is.EqualTo(0));
+		}
+
+		[Test]
+		public void WinnerAsSourceReturns100Test()
+		{
+			Mock<Board> mockBoard = new Mock<Board> { CallBase = true };
+			mockBoard.Setup(x => x.GetAvailableMoves())
+				.Returns(new List<Tuple<int, int>>());
+			mockBoard.Setup(x => x.GetWinner()).Returns(new Nought());
+			mockBoard.Setup(x => x.NextMove).Returns(new Nought());
+
+			int returnValue = Minimax.minimax(mockBoard.Object, new Nought());
+
+			Assert.That(returnValue, Is.EqualTo(100));
+		}
+	}
+}
